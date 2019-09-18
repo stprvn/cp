90826056dee6260aeeeac88c138e3c9d27a81199
Stephanus Parveen <stparveen.14116128@student.itera.ac.id>
2019-09-18 22:06:11 +0700

    Add files via upload

---
+++ CAESARCHlPER.cpp
@@ -0,0 +1,72 @@
+/*Algorithm caesar chiper classic to encrypt plaintext -> chipertext  
+									decrypt chipertext -> plaintext
+  I learn this from cryptho class 
+Input : string (A-Z)/(a-z)
+max_size input -> string
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void encr(int key,string kata){
+	//plaintext to chipertext
+	int ps;
+	for(int i=0;i<kata.length();i++){
+		ps = key + (kata[i]-'0');		
+		if(isupper(kata[i])){
+			if(ps<=42){
+				kata[i]+=key;
+			}else{
+				kata[i] = 'A' + (ps % 42) - 1;
+			}
+		}else{
+			if(ps<=74){
+				kata[i]+=key;
+			}else{
+				kata[i] = 'a' + (ps % 74) - 1;
+			}
+		}
+		
+	}
+	cout<<kata;
+}
+
+void decr(int key,string kata){
+	//chipertext to plaintext
+	int ps;
+	for(int i=0;i<kata.length();i++){
+		ps = (kata[i]-'0') - key;	
+		if(isupper(kata[i])){
+			if(ps>=17){
+				kata[i]-=key;
+			}else{
+				kata[i] = 'Z' - (17 % ps) + 1;
+			}
+		}else{
+			if(ps>=49){
+				kata[i]-=key;
+			}else{
+				kata[i] = 'z' - (49 % ps) + 1;
+			}
+		}	
+	}
+	cout<<kata;
+
+}
+int main(){
+	int rot,pil;
+	string kata;
+
+	cout<<"1. encrypt ?\n2. decrypt ?\nChoice : ";
+	cin>>pil;
+	cout<<"Key  : ";
+	cin>>rot;
+	cout<<"Text :";
+	cin.ignore();
+	getline(cin,kata);
+	rot%=26;
+	(pil==1) ? encr(rot,kata) : decr(rot,kata) ;
+	
+
+	return 0;
+}
